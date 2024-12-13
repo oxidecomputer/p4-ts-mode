f84635ee6dac54f0ff34ee3b7b59e4db59275c9a
Zeeshan Lakhani <zeeshan.lakhani@gmail.com>
2024-12-13 16:49:20 -0500

    add links to header

--- p4-ts-mode.el
+++ p4-ts-mode.el
@@ -18,8 +18,11 @@
 
 ;;; Commentary:
 
-;; Provides syntax highlighting and indentation for the P4 (P4_16-specification))
+;; Provides syntax highlighting and indentation for the P4 (P4_16-specification)
 ;; domain-specific programming language.
+;;
+;; P4: https://p4.org/
+;; P4_16-specification: https://p4.org/p4-spec/docs/P4-16-v1.2.0-spec.html
 
 ;; Inspiration comes from the gleam tree-sitter implementation
 ;; (https://github.com/gleam-lang/gleam-mode/blob/main/gleam-ts-mode.el)
